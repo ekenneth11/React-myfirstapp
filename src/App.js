87a75b5931471b7ed9f0ce94f2efa9ddb665e6fd
Kenneth <kennethbautista1512@gmail.com>
2025-02-18 10:03:31 -0500

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+function App(props) {
+  const currDate = new Date();
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, Kenneth!</h1>
+      <h2>Today is {currDate.toLocaleDateString()} and the time is {currDate.toLocaleTimeString()}.</h2>
     </div>
   );
 }
